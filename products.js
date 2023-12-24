90f8e45d5157271c987f62dd7fc52f50238b9481
Nidhi Verma <66475609+Nidhiverma91@users.noreply.github.com>
2023-12-24 18:45:52 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,40 @@
+const products = [
+  {
+    id: 1,
+    name: "White shoe",
+    brand: "Nike",
+    desc: "Shoe number - 6",
+    price: 1000,
+    image:
+    "http://res.cloudinary.com/djzhfz7lm/samples/shoe.jpg",
+  },
+  {
+    id: 2,
+    name: "Chair and Table",
+    brand: "Ikia",
+    desc: "Brown and Yellow colour",
+    price: 500,
+    image:
+      "http://res.cloudinary.com/djzhfz7lm/samples/chair-and-coffee-table.jpg",
+  },
+  {
+    id: 3,
+    name: "Bike",
+    brand: "Hero",
+    desc: "BIG Size",
+    price: 800,
+    image:
+      "https://res.cloudinary.com/djzhfz7lm/samples/bike.jpg",
+  },
+  {
+    id: 3,
+    name: "Phone",
+    brand: "Iphone",
+    desc: "New Model",
+    price: 1100,
+    image:
+      "https://res.cloudinary.com/dgqxrbs6l/samples/b9iks1j9x2xicagvzf3r.jpg",
+  },
+];
+
+module.exports = products;
